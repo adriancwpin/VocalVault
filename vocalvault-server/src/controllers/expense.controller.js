0e07da549337028dfbeafd63cdd4dac195e97d19
adriancwpin <adriancwpin8@gmail.com>
2026-06-26 09:12:23 +0100

    Done with creating expenses route

--- vocalvault-server/src/controllers/expense.controller.js
+++ vocalvault-server/src/controllers/expense.controller.js
@@ -1,6 +1,26 @@
 //call get all expense from model and response the request info 
 
-import { getAllExpenses } from "../models/expense.model.js";
+import { getAllExpenses, createExpense } from "../models/expense.model.js";
+
+async function createExpenseHandler(req,res){
+    try{
+        const {amount,categoryId, description, rawText, source} = req.body;
+
+        const newExpense = await createExpense({amount, categoryId, description, rawText, source});
+        
+        //201 - created successfully
+        return res.status(201).json({
+            success: true,
+            data: newExpense
+        });
+    }catch (err){
+        console.error("Error creating expenses: ", err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error. Could not create expense."
+        });
+    }
+}
 
 async function getAllExpensesHandler(req, res){
     try{
@@ -23,4 +43,4 @@ async function getAllExpensesHandler(req, res){
     }
 }
 
-export { getAllExpensesHandler };
+export { getAllExpensesHandler, createExpenseHandler };
--- vocalvault-server/src/routes/expense.route.js
+++ vocalvault-server/src/routes/expense.route.js
@@ -1,9 +1,11 @@
 //map the URL and HTTP method to the controller 
 import express from 'express' ;
-import { getAllExpensesHandler } from '../controllers/expense.controller.js';
+import { getAllExpensesHandler, createExpenseHandler } from '../controllers/expense.controller.js';
 
 const router = express.Router(); //creates a self contained set of routes
 
+router.post('/', createExpenseHandler);
 router.get('/' , getAllExpensesHandler); // -> GET/api/expenses
+
 
 export default router;
