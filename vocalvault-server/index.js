bdbe816fe5ea1adedc5605e007b5cf1df3609fea
adriancwpin <adriancwpin8@gmail.com>
2026-06-27 05:12:36 +0100

    done with checking category router

--- vocalvault-server/index.js
+++ vocalvault-server/index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(express.json());
 
 app.use('/api/expenses', expensesRouter);
-app.use('/api/categories', categoriesRouter);
+app.use('/api/categories', categoryRouter);
 
 app.get('/api/health', (req,res) => {
     res.json({status: 'ok'});
